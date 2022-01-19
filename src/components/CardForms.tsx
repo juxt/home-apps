278e2bffca42b61482739a7d0eaee8b754bf03c6
Alex Davis <alx@juxt.pro>
2022-01-19 15:33:37 +0000

    reorder card form components for better keyboard nav

--- src/components/CardForms.tsx
+++ src/components/CardForms.tsx
@@ -195,36 +195,6 @@ export function UpdateCardModal({ isOpen, setIsOpen }: UpdateCardModalProps) {
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
       <div className="relative">
-        <div className="absolute top-7 sm:top-5 right-4">
-          <OptionsMenu
-            options={[
-              {
-                label: "Delete",
-                id: "delete",
-                props: {
-                  onClick: () => {
-                    setIsOpen(false);
-                    if (card?.id) {
-                      toast.promise(
-                        updateCardMutation.mutateAsync({
-                          cardId: card.id,
-                          card: {
-                            projectId: null,
-                          },
-                        }),
-                        {
-                          pending: "Deleting card...",
-                          success: "Card deleted!",
-                          error: "Error deleting card",
-                        }
-                      );
-                    }
-                  },
-                },
-              },
-            ]}
-          />
-        </div>
         <Form
           title={title}
           formHooks={formHooks}
@@ -263,22 +233,52 @@ export function UpdateCardModal({ isOpen, setIsOpen }: UpdateCardModalProps) {
           ]}
           onSubmit={formHooks.handleSubmit(updateCard, console.warn)}
         />
-      </div>
-      <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-        <button
-          type="submit"
-          form={title}
-          className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-        >
-          Submit
-        </button>
-        <button
-          type="button"
-          className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-          onClick={() => setIsOpen(false)}
-        >
-          Cancel
-        </button>
+        <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+          <button
+            type="submit"
+            form={title}
+            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+          >
+            Submit
+          </button>
+          <button
+            type="button"
+            className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+            onClick={() => setIsOpen(false)}
+          >
+            Cancel
+          </button>
+        </div>
+        <div className="absolute top-7 sm:top-5 right-4">
+          <OptionsMenu
+            options={[
+              {
+                label: "Delete",
+                id: "delete",
+                props: {
+                  onClick: () => {
+                    setIsOpen(false);
+                    if (card?.id) {
+                      toast.promise(
+                        updateCardMutation.mutateAsync({
+                          cardId: card.id,
+                          card: {
+                            projectId: null,
+                          },
+                        }),
+                        {
+                          pending: "Deleting card...",
+                          success: "Card deleted!",
+                          error: "Error deleting card",
+                        }
+                      );
+                    }
+                  },
+                },
+              },
+            ]}
+          />
+        </div>
       </div>
     </Modal>
   );
