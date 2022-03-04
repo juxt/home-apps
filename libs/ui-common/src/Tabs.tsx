9634a61300a07afee3cbe011b923d256227c6a57
Alex Davis <alx@juxt.pro>
2022-03-04 14:32:58 +0000

    highlight search when something is entered

--- libs/ui-common/src/Tabs.tsx
+++ libs/ui-common/src/Tabs.tsx
@@ -70,7 +70,7 @@ export function NavTabs({ tabs, navName }: TabProps) {
                 <label htmlFor="search" className="sr-only">
                   Search
                 </label>
-                <div className="relative">
+                <div className="relative ">
                   <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                     <SearchIcon
                       className="h-5 w-5 text-gray-400"
@@ -80,7 +80,12 @@ export function NavTabs({ tabs, navName }: TabProps) {
                   <input
                     id="search"
                     name="search"
-                    className="block pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    className={classNames(
+                      searchVal && searchVal.length > 0
+                        ? 'border-indigo-600'
+                        : 'border-gray-300',
+                      'block pl-10 pr-3 py-2 border rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+                    )}
                     placeholder="Search"
                     type="search"
                     onChange={(e) => setSearchVal(e.target.value)}
