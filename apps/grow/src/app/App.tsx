983dd3985ea6d806bccb4b2d3f5e585bdb3e0347
Alex Davis <alx@juxt.pro>
2022-03-17 12:13:58 +0000

    Add edit mode to item view

--- apps/grow/src/app/App.tsx
+++ apps/grow/src/app/App.tsx
@@ -1,77 +1,18 @@
-import {
-  CreateProgressMutationVariables,
-  useAllProgressQuery,
-  useCreateProgressMutation,
-} from '@juxt-home/site';
-import { StandaloneForm, TipTapContent, useDirty } from '@juxt-home/ui-common';
+import { useAllProgressQuery } from '@juxt-home/site';
+import { TipTapContent } from '@juxt-home/ui-common';
 import { notEmpty } from '@juxt-home/utils';
-import { useForm } from 'react-hook-form';
 import { Link } from 'react-location';
-import { useQueryClient } from 'react-query';
 import './App.css';
 
 export function App() {
-  console.log('App');
-
   const { data, error, isError, isLoading, isLoadingError } =
     useAllProgressQuery();
-  const queryClient = useQueryClient();
-  const { mutate, error: mutationError } = useCreateProgressMutation({
-    onSettled: () => {
-      queryClient.refetchQueries('allProgress');
-    },
-  });
-  const defaultValues = {
-    progressItem: {
-      name: 'hello',
-      currentScore: undefined,
-    },
-  };
-  const formHooks = useForm<CreateProgressMutationVariables>({
-    defaultValues,
-  });
 
-  const handleSubmit = async (
-    values: Omit<CreateProgressMutationVariables, 'id'>,
-  ) => {
-    console.log('submit', values);
-    formHooks.reset(defaultValues);
-    mutate({
-      progressItem: {
-        ...values.progressItem,
-        id: values.progressItem.name,
-      },
-    });
-  };
   return (
     <div className="mx-10 py-20">
-      <StandaloneForm
-        fields={[
-          {
-            name: 'name',
-            label: 'Name',
-            type: 'text',
-            path: 'progressItem.name',
-          },
-          {
-            label: 'Description',
-            type: 'tiptap',
-            path: 'progressItem.descriptionHTML',
-          },
-          {
-            label: 'Progress',
-            type: 'text',
-            path: 'progressItem.currentScore',
-          },
-        ]}
-        formHooks={formHooks}
-        handleSubmit={formHooks.handleSubmit(handleSubmit)}
-      />
       {isLoading && <div>Loading...</div>}
       {isError && <div>Error: {error?.message}</div>}
       {isLoadingError && <div>Loading error: {error?.message}</div>}
-      <pre>{JSON.stringify(error)}</pre>
-      <pre>{JSON.stringify(mutationError)}</pre>
       {data?.allProgressItems &&
         data.allProgressItems
           .filter(notEmpty)
--- apps/grow/src/item/Item.tsx
+++ apps/grow/src/item/Item.tsx
@@ -1,6 +1,69 @@
-import { useAllProgressQuery } from '@juxt-home/site';
+import {
+  useAllProgressQuery,
+  AllProgressQuery,
+  CreateProgressMutationVariables,
+  useCreateProgressMutation,
+} from '@juxt-home/site';
+import { Button, StandaloneForm } from '@juxt-home/ui-common';
 import { notEmpty } from '@juxt-home/utils';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useMatch } from 'react-location';
+import { useQueryClient } from 'react-query';
+
+type TProgressItem = NonNullable<
+  NonNullable<AllProgressQuery['allProgressItems']>[0]
+>;
+
+function ProgressItem({ progressItem }: { progressItem: TProgressItem }) {
+  return (
+    <div>
+      <h1>{progressItem.name}</h1>
+      <p>{progressItem.descriptionHTML}</p>
+      <p>{progressItem.currentScore}</p>
+    </div>
+  );
+}
+
+function ProgressItemEdit({
+  progressItem,
+  handleSubmit,
+}: {
+  progressItem: TProgressItem;
+  handleSubmit: (values: CreateProgressMutationVariables) => void;
+}) {
+  const defaultValues = {
+    progressItem,
+  };
+  const formHooks = useForm<CreateProgressMutationVariables>({
+    defaultValues,
+  });
+
+  return (
+    <StandaloneForm
+      fields={[
+        {
+          name: 'name',
+          label: 'Name',
+          type: 'text',
+          path: 'progressItem.name',
+        },
+        {
+          label: 'Description',
+          type: 'tiptap',
+          path: 'progressItem.descriptionHTML',
+        },
+        {
+          label: 'Progress',
+          type: 'text',
+          path: 'progressItem.currentScore',
+        },
+      ]}
+      formHooks={formHooks}
+      handleSubmit={formHooks.handleSubmit(handleSubmit)}
+    />
+  );
+}
 
 export default function Item() {
   const { data, error, isError, isLoading, isLoadingError } =
@@ -12,21 +75,36 @@ export default function Item() {
 
   const progressItem = data?.allProgressItems
     ?.filter(notEmpty)
-    .find((pItem) => pItem.name === item);
-  console.log('progressItem', progressItem);
+    .find((pItem) => pItem.id === item);
 
+  const [editMode, setEditMode] = useState(false);
+  const Progress = editMode ? ProgressItemEdit : ProgressItem;
+  const queryClient = useQueryClient();
+  const { mutate, error: mutationError } = useCreateProgressMutation({
+    onSettled: () => {
+      queryClient.refetchQueries('allProgress');
+    },
+  });
+  const handleSubmit = async (values: CreateProgressMutationVariables) => {
+    console.log('submit', values);
+    setEditMode(false);
+    mutate({
+      progressItem: values.progressItem,
+    });
+  };
   return (
     <div>
       {isLoading && <div>Loading...</div>}
       {isError && <div>Error: {error?.message}</div>}
       {isLoadingError && <div>Loading error: {error?.message}</div>}
+      <Button onClick={() => setEditMode(!editMode)}>
+        {editMode ? 'Stop Editing' : 'Edit'}
+      </Button>
+
       {progressItem && (
-        <div>
-          <h1>{progressItem.name}</h1>
-          <p>{progressItem.descriptionHTML}</p>
-          <p>{progressItem.currentScore}</p>
-        </div>
+        <Progress handleSubmit={handleSubmit} progressItem={progressItem} />
       )}
+
       {!isLoading && !isError && !isLoadingError && !progressItem && (
         <div>No progress item found</div>
       )}
