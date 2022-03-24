e7be8ff5e749b55bd9724ec5101cf2daf0d34a5e
IAmNini <fortuny.carrega@gmail.com>
2022-03-24 16:18:37 +0000

    update search results page for mobile

--- apps/film-and-tv-ratings/src/pages/SearchResults.tsx
+++ apps/film-and-tv-ratings/src/pages/SearchResults.tsx
@@ -2,12 +2,14 @@ import {
   Link,
   Outlet,
   useMatch,
+  useMatches,
   useNavigate,
+  useRouter,
   useSearch,
 } from '@tanstack/react-location';
 import { useAtom } from 'jotai';
 import { useQuery } from 'react-query';
-import { api_key, client, poster_img } from '../common';
+import { api_key, client, PosterImage } from '../common';
 import { TMDBError } from '../components/Errors';
 import { searchAtom } from '../components/Search';
 import {
@@ -16,7 +18,16 @@ import {
   TSearchResults,
   TSearchType,
 } from '../types';
-import { Card, Image, Text, SimpleGrid, Pagination, Title } from '@mantine/core';
+import {
+  Card,
+  Image,
+  Text,
+  SimpleGrid,
+  Pagination,
+  Title,
+  Button,
+} from '@mantine/core';
+import { useMobileDetect } from '@juxt-home/utils';
 
 async function searchQuery(
   searchTerm: string,
@@ -45,9 +56,9 @@ function useSearchResults(
 
 export function SearchResults() {
   const navigate = useNavigate<NavStructure>();
-  const {
-    params: { searchType },
-  } = useMatch<NavStructure>();
+  const matches = useMatches<NavStructure>();
+  const searchType = matches[0].params?.searchType;
+  const itemId = matches[1]?.params?.itemId;
   const { page } = useSearch<NavStructure>();
   const [search] = useAtom(searchAtom);
 
@@ -61,54 +72,72 @@ export function SearchResults() {
     });
   };
 
-  console.log(response.data?.results);
-  
+  const isMobile = useMobileDetect().isMobile();
+  const showBackButton = isMobile && itemId;
   return (
     <div>
-      {search && (
+      {search && showBackButton ? (
+        <>
+          <Button
+            onClick={() =>
+              navigate({
+                to: '.',
+                search: (old) => ({ ...old, query: search }),
+              })
+            }>
+            Back Button
+          </Button>
+          <Outlet />
+        </>
+      ) : (
         <SimpleGrid cols={2}>
           <div>
-          <Title order={1}>Search Results</Title>
-          <Text color="gray">You are searching in {searchType}</Text>
-          <Text color="gray">Showing results for "{search}"</Text>
-          {response.isLoading && <p>loading...</p>}
-          {response.isError && <TMDBError error={response.error} />}
-          {response.isSuccess && (
-            <>
-              <Pagination total={10} color="orange" size="sm" radius="xs" onChange={handleChangePage}/>
-              <SimpleGrid cols={4}>
-                {response.data.results?.map((result) => (
-                  <div key={result.id}>
-                    <Link to={`/search/${searchType}/${result.id}`}>
-                      <Card
-                      shadow="sm"
-                      p="xl"
-                      // component="a"
-                      // href={`/search/${searchType}/${result.id}`}
-                      // target="_blank"
-                      >
-                      <Card.Section>
-                        <Image src={`https://image.tmdb.org/t/p/original/${result.poster_path}`} height={160} alt="Movie poster" />
-                      </Card.Section>
+            <Title order={1}>Search Results</Title>
+            <Text color="gray">You are searching in {searchType}</Text>
+            <Text color="gray">Showing results for "{search}"</Text>
+            {response.isLoading && <p>loading...</p>}
+            {response.isError && <TMDBError error={response.error} />}
+            {response.isSuccess && (
+              <>
+                <Pagination
+                  total={10}
+                  color="orange"
+                  size="sm"
+                  radius="xs"
+                  page={page}
+                  onChange={handleChangePage}
+                />
+                <SimpleGrid cols={4}>
+                  {response.data.results?.map((result) => (
+                    <div key={result.id}>
+                      <Link to={`/search/${searchType}/${result.id}`}>
+                        <Card
+                          shadow="sm"
+                          p="xl"
+                          // component="a"
+                          // href={`/search/${searchType}/${result.id}`}
+                          // target="_blank"
+                        >
+                          <Card.Section>
+                            <PosterImage posterPath={result.poster_path} />
+                          </Card.Section>
 
-                      <Text weight={500} size="lg">
-                      {result?.title || result?.name}
-                      </Text>
+                          <Text weight={500} size="lg">
+                            {result?.title || result?.name}
+                          </Text>
 
-                      {/* <Text size="sm">
+                          {/* <Text size="sm">
                         Please click anywhere on this card to claim your reward,
                         this is not a fraud, trust us
                       </Text> */}
-                    </Card>
-                    </Link>
-                  </div>
-                ))}
-              </SimpleGrid>
-            </>
-          )}
+                        </Card>
+                      </Link>
+                    </div>
+                  ))}
+                </SimpleGrid>
+              </>
+            )}
           </div>
-          {/* 'Outlet' renders the remaining route matches from the router.
-       In this case it will be where the /:itemId route is rendered */}
           <Outlet />
         </SimpleGrid>
       )}
