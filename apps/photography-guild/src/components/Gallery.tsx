e32b780aa5d8aa4bc251c484da256d9d7c3d989e
Lucio D'Alessandro <lda@juxt.pro>
2022-04-24 01:16:28 +0200

    Remove stroke attr from star svg

--- apps/photography-guild/src/components/Gallery.tsx
+++ apps/photography-guild/src/components/Gallery.tsx
@@ -121,8 +121,7 @@ function StarIcon() {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       className="h-5 w-5 fill-gray-700"
-      viewBox="0 0 24 24"
-      strokeWidth={2}>
+      viewBox="0 0 24 24">
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
