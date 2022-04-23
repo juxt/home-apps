ec404774ef37d8c83c2c723fb49ab839b45443b3
Lucio D'Alessandro <lda@juxt.pro>
2022-04-24 01:15:06 +0200

    Add startZzZzZz

--- apps/photography-guild/src/components/Gallery.tsx
+++ apps/photography-guild/src/components/Gallery.tsx
@@ -116,6 +116,32 @@ function ExifInfo({ exifData }: { exifData: ExifType }) {
   );
 }
 
+function StarIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-5 w-5 fill-gray-700"
+      viewBox="0 0 24 24"
+      strokeWidth={2}>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+      />
+    </svg>
+  );
+}
+
+function RatingStars({ rating }: { rating: number }) {
+  const ratings = [...Array(5).keys()].map((x) => x + 1);
+
+  return (
+    <div className="flex flex-row-reverse">
+      {ratings.map((x) => rating >= x && <StarIcon />)}
+    </div>
+  );
+}
+
 export function Gallery() {
   const { data } = useAllPhotosQuery();
   const { id: userId } = useUser();
@@ -136,7 +162,7 @@ export function Gallery() {
     <div className="flex flex-wrap gap-1">
       {data?.allPhotos
         ?.filter(notEmpty)
-        .map(({ publicId, exif, _siteSubject, id, imageUrl }) => {
+        .map(({ publicId, exif, _siteSubject, imageUrl, rating }) => {
           const exifData: ExifType = exif && JSON.parse(exif);
 
           return (
@@ -158,6 +184,11 @@ export function Gallery() {
                         'cursor-pointer',
                       )}>
                       <ExifInfo exifData={exifData} />
+                      {rating && (
+                        <div className="p-2 absolute bottom-0 right-0 w-full flex justify-end">
+                          <RatingStars rating={rating} />
+                        </div>
+                      )}
                     </div>
                   </div>
                   {/* { <OptionsMenu
