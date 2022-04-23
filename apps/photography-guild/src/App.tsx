4a5b87ef45f0372a11dc003e31a0e3bf6e254566
Lucio D'Alessandro <lda@juxt.pro>
2022-04-23 23:27:49 +0200

    Add nav and improve gallery WIP

--- apps/photography-guild/src/App.tsx
+++ apps/photography-guild/src/App.tsx
@@ -1,37 +1,29 @@
-import { useUser } from '@juxt-home/site';
-import {
-  DeleteActiveIcon,
-  DeleteInactiveIcon,
-  OptionsMenu,
-} from '@juxt-home/ui-common';
-import { notEmpty } from '@juxt-home/utils';
+import { Navbar } from '@juxt-home/ui-common';
 import { useState } from 'react';
-import { useQueryClient } from 'react-query';
-import { toast } from 'react-toastify';
-import {
-  CloudinaryImage,
-  CloudinaryUploadWidget,
-} from './components/Cloudinary';
+import { CloudinaryUploadWidget } from './components/Cloudinary';
 import { ImageForm } from './components/Form';
 import { Gallery } from './components/Gallery';
 import { CloudinaryImageFields } from './components/types';
-import { useAllPhotosQuery, useDeletePhotoMutation } from './generated/graphql';
 
 export function App() {
   const [cloudinaryImage, setCloudinaryImage] =
     useState<CloudinaryImageFields>();
 
   return (
-    <div className="max-w-xl mx-auto prose">
-      <h1>my photos</h1>
+    <div>
+      <Navbar />
 
-      <CloudinaryUploadWidget setCloudinaryImage={setCloudinaryImage} />
-      <ImageForm
-        setCloudinaryImage={setCloudinaryImage}
-        cloudinaryImage={cloudinaryImage}
-      />
+      <div className="max-w-7xl mx-auto">
+        <h1>my photos</h1>
 
-      <Gallery />
+        <CloudinaryUploadWidget setCloudinaryImage={setCloudinaryImage} />
+        <ImageForm
+          setCloudinaryImage={setCloudinaryImage}
+          cloudinaryImage={cloudinaryImage}
+        />
+
+        <Gallery />
+      </div>
     </div>
   );
 }
