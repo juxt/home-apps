33b95895015bf335f832d71e4d721377491764a2
Alex Davis <alx@juxt.pro>
2022-04-24 01:13:37 +0100

    add alternate magnifier

--- apps/photography-guild/src/components/PhotoPage.tsx
+++ apps/photography-guild/src/components/PhotoPage.tsx
@@ -1,12 +1,7 @@
-import {
-  GlassMagnifier,
-  Magnifier,
-  MOUSE_ACTIVATION,
-  TOUCH_ACTIVATION,
-} from 'react-image-magnifiers';
+import { useState } from 'react';
+import { GlassMagnifier, Magnifier } from 'react-image-magnifiers';
 import { cloudName } from '../constants';
 import { useAllPhotosQuery } from '../generated/graphql';
-import { ExifType } from './types';
 
 export function PhotoPage({ id }: { id?: string }) {
   const { data: photoData } = useAllPhotosQuery(undefined, {
@@ -15,18 +10,46 @@ export function PhotoPage({ id }: { id?: string }) {
   });
   const exif = photoData?.exif && JSON.parse(photoData?.exif);
   const thumbnailUrl = `https://res.cloudinary.com/${cloudName}/image/upload/c_thumb,w_400,h_350/${photoData?.publicId}`;
+  const [magnifierType, setMagnifierType] = useState<'glass' | 'magnifier'>(
+    'glass',
+  );
 
   return (
     <div className="flex flex-col items-center">
       <h1>Photo Page</h1>
       <p>Photo id: {photoData?.id}</p>
+      <button
+        className={magnifierType === 'glass' ? 'opacity-50' : ''}
+        onClick={() => setMagnifierType('glass')}
+        disabled={magnifierType === 'glass'}
+        type="button">
+        Glass magnifier
+      </button>
+      <button
+        className={magnifierType === 'magnifier' ? 'opacity-50' : ''}
+        onClick={() => setMagnifierType('magnifier')}
+        disabled={magnifierType === 'magnifier'}
+        type="button">
+        Click to Magnify magnifier
+      </button>
       {photoData?.imageUrl && (
-        <Magnifier
-          className="relative w-1/2"
-          imageAlt={photoData?.title || id}
-          imageSrc={thumbnailUrl}
-          largeImageSrc={photoData?.imageUrl}
-        />
+        <div className="flex flex-col items-center relative w-1/2">
+          {magnifierType === 'glass' && (
+            <GlassMagnifier
+              imageSrc={photoData.imageUrl}
+              largeImageSrc={photoData.imageUrl}
+              magnifierSize="200"
+              cursorStyle="crosshair"
+            />
+          )}
+          {magnifierType === 'magnifier' && (
+            <Magnifier
+              imageAlt={photoData?.title || id}
+              imageSrc={thumbnailUrl}
+              largeImageSrc={photoData?.imageUrl}
+            />
+          )}
+        </div>
       )}
     </div>
   );
